73b4fe0aa1130d97df6780bba75174c051d0501c
Bill-Haku <battleandwar@hotmail.com>
2023-01-12 19:22:12 +0800

    code: del unused commands

--- main.py
+++ main.py
@@ -64,13 +64,6 @@ config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 bot = IrminsulTerminalBot()
 
 
-@bot.command(name="paimon")
-async def paimon(ctx):
-    terminal = IrminsulTerminal(language="en")
-    _log.info(f"Recognized command paimon from {ctx.author.name} #{ctx.author.id}")
-    await ctx.send(embed=terminal.selfInfo(bot=bot))
-
-
 @bot.tree.command(name="about")
 async def aboutCmd(interaction: discord.Interaction):
     _log.info(f"Recognized command about from {interaction.user.name} #{interaction.user.id}")
@@ -79,77 +72,12 @@ async def aboutCmd(interaction: discord.Interaction):
     await interaction.response.send_message(embed=terminal.selfInfo(bot=bot))
 
 
-@bot.command(name="关于")
-async def paimon(ctx):
-    terminal = IrminsulTerminal(language="zh")
-    _log.info(f"Recognized command 关于 from {ctx.author.name} #{ctx.author.id}")
-    await ctx.send(embed=terminal.selfInfo(bot=bot))
-
-
-@bot.command(name="ボット")
-async def paimon(ctx):
-    terminal = IrminsulTerminal(language="ja")
-    _log.info(f"Recognized command ボット from {ctx.author.name} #{ctx.author.id}")
-    await ctx.send(embed=terminal.selfInfo(bot=bot))
-
-
-@bot.command(name="menu")
-async def menuTest(ctx):
-    _log.info(f"Recognized command menuTest from {ctx.author.name} #{ctx.author.id}")
-    view = View()
-    button1 = Button(label="Button 1", style=ButtonStyle.red)
-    button2 = Button(label="Button 2", style=ButtonStyle.gray)
-    button3 = Button(label="Button 3", style=ButtonStyle.green)
-
-    async def on_button_click(interaction: discord.Interaction):
-        _log.info(f"{interaction.id} clicked")
-        await interaction.response.send_message(f"{interaction.id} clicked")
-
-    button1.callback = on_button_click
-    button2.callback = on_button_click
-    button3.callback = on_button_click
-
-    view.add_item(button1)
-    view.add_item(button2)
-    view.add_item(button3)
-    await ctx.send("Menu test", view=view)
-
-
-@bot.command(name="bind")
-async def bindUID(ctx, uid):
-    _log.info(f"Recognized command bind {uid} from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="en")
-    await ctx.send(terminal.bindUID(user=ctx.author, uid=uid) + uid)
-
-
 @bot.tree.command(name="bind")
 async def bindUIDCmd(interaction: discord.Interaction, uid: str) -> None:
     _log.info(f"Recognized command bind {uid} from {interaction.user.name} #{interaction.user.id}")
     _, language = IrminsulDatabase.lookUpLanguage(user_id=interaction.user.id)
     terminal = IrminsulTerminal(language=language)
     await interaction.response.send_message(terminal.bindUID(user=interaction.user, uid=uid) + str(uid))
-
-
-@bot.command(name="绑定")
-async def bindUID(ctx, uid):
-    _log.info(f"Recognized command bind {uid} from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="zh")
-    await ctx.send(terminal.bindUID(user=ctx.author, uid=uid) + uid)
-
-
-@bot.command(name="バインド")
-async def bindUID(ctx, uid):
-    _log.info(f"Recognized command bind {uid} from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="ja")
-    await ctx.send(terminal.bindUID(user=ctx.author, uid=uid) + uid)
-
-
-@bot.command(name="lookup")
-async def lookup(ctx):
-    _log.info(f"Recognized command lookup from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="en")
-    resTitle, resView = terminal.lookUpHandler()
-    await ctx.send(resTitle, view=resView)
 
 
 @bot.tree.command(name="lookup")
@@ -161,36 +89,12 @@ async def lookUpCmd(interaction: discord.Interaction) -> None:
     await interaction.response.send_message(resTitle, view=resView)
 
 
-@bot.command(name="検索")
-async def lookup(ctx):
-    _log.info(f"Recognized command lookup from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="ja")
-    resTitle, resView = terminal.lookUpHandler()
-    await ctx.send(resTitle, view=resView)
-
-
-@bot.command(name="查询")
-async def lookup(ctx):
-    _log.info(f"Recognized command lookup from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="zh")
-    resTitle, resView = terminal.lookUpHandler()
-    await ctx.send(resTitle, view=resView)
-
-
 @bot.tree.command(name="setlanguage")
 async def setLanguage(interaction: discord.Interaction, language: str) -> None:
     _log.info(f"Recognized command setlanguage from {interaction.user.name} #{interaction.user.id}")
     terminal = IrminsulTerminal(language=language)
     res = terminal.setLanguage(user=interaction.user, language=language)
     await interaction.response.send_message(res)
-
-
-@bot.command(name="char")
-async def lookUpChar(ctx, charName):
-    _log.info(f"Recognized command look up from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="en")
-    resTitle, resView = terminal.lookUpChar(ctx.author.id, charName)
-    await ctx.send(resTitle, view=resView)
 
 
 @bot.tree.command(name="character")
@@ -219,22 +123,6 @@ async def helpCmd(interaction: discord.Interaction):
     terminal = IrminsulTerminal(language=language)
     resView = terminal.sendManual()
     await interaction.response.send_message(view=resView)
-
-
-@bot.command(name="角色")
-async def lookUpChar(ctx, charName):
-    _log.info(f"Recognized command look up from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="zh")
-    resTitle, resView = terminal.lookUpChar(ctx.author.id, charName)
-    await ctx.send(resTitle, view=resView)
-
-
-@bot.command(name="キャラ")
-async def lookUpChar(ctx, charName):
-    _log.info(f"Recognized command look up from {ctx.author.name} #{ctx.author.id}")
-    terminal = IrminsulTerminal(language="ja")
-    resTitle, resView = terminal.lookUpChar(ctx.author.id, charName)
-    await ctx.send(resTitle, view=resView)
 
 
 @bot.command(name="createvc")
